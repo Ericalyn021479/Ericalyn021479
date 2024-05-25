4cb81955f764bbed330e854fd2eb2360bc4e0e7f
Ericalyn021479 <ericalyn_021479@binalatongan.edu.ph>
2024-05-25 14:50:52 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const btnLike1= document.getElementById("btnLike1")
+const countLike = document.getElementById("countLike")
+
+function clickLike(){
+  let totalLikes1 = parseInt(countalikes1.value)+1
+  countLikes1.textContent = totalLikes1.toString()
+}
+htmlLike1.addEventListener("click",clickLike1)
